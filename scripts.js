7eade1dae19ddb04b7e5be3f63d7739339efcf7d
frescodicredito <dicredicofrancesco@gmail.com>
2024-12-02 10:08:06 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetID = this.getAttribute('href');
+    const targetElement = document.querySelector(targetID);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  });
+});
